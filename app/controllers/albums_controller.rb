d6cfc59877601be19224073c66a40935f875a7d1
Matthieu DESILE <matthieu.desile@gmail.com>
2010-02-11 11:59:12 +0100

    will_paginate

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,7 +2,7 @@ class AlbumsController < ApplicationController
   # GET /albums
   # GET /albums.xml
   def index
-    @albums = Album.all
+    @albums = Album.paginate :page => params[:page]
 
     respond_to do |format|
       format.html # index.html.erb
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   # GET /images
   # GET /images.xml
   def index
-    @images = Image.all
+    @images = Image.paginate :page => params[:page]
 
     respond_to do |format|
       format.html # index.html.erb
