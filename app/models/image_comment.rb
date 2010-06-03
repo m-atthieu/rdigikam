12f090ef3555d1b3cf70ac75b4f3a382f13561c0
Matthieu DESILE <matthieu.desile@gmail.com>
2010-06-03 10:55:54 +0200

    inheritance column type

--- app/models/image_comment.rb
+++ app/models/image_comment.rb
@@ -1,4 +1,5 @@
 class ImageComment < DigikamDb
 	set_table_name "imagecomments"
 	belongs_to :image, :foreign_key => "imageid"
+    set_inheritance_column :ruby_type
 end
--- app/models/image_copyright.rb
+++ app/models/image_copyright.rb
@@ -1,4 +1,5 @@
 class ImageCopyright < DigikamDb
-	set_table_name "imagecopyright"
-	belongs_to :image, :foreign_key => "imageid"
+  set_table_name "imagecopyright"
+  set_inheritance_column :ruby_type
+  belongs_to :image, :foreign_key => "imageid"
 end
