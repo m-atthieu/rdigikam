5ce88a1cce32a34b5b819c17109c2b9fd1664934
Matthieu DESILE <matthieu.desile@gmail.com>
2010-06-08 17:32:55 +0200

    do not want to IGNORECASE

--- config/initializers/sqlite3_regexp.rb
+++ config/initializers/sqlite3_regexp.rb
@@ -3,9 +3,9 @@ require 'active_record/connection_adapters/sqlite3_adapter'
 
 class ActiveRecord::ConnectionAdapters::SQLite3Adapter
   def initialize(db, logger, config)
-    super
+    super db, logger, config
     db.create_function('regexp', 2) do |func, pattern, expression|
-       regexp = Regexp.new(pattern.to_s, Regexp::IGNORECASE)
+       regexp = Regexp.new(pattern.to_s)
        if expression.to_s.match(regexp)
          func.result = 1
        else
