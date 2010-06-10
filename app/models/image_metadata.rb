c6e4a942e9c586354d566b8ea55f5f62564b54a1
Matthieu DESILE <matthieu.desile@gmail.com>
2010-06-10 14:39:38 +0200

    image metadata json generation

--- app/models/image_metadata.rb
+++ app/models/image_metadata.rb
@@ -2,4 +2,7 @@ class ImageMetadata < DigikamDb
 	set_table_name "imagemetadata"
 	set_primary_key "imageid"
 	belongs_to :image, :foreign_key => "imageid"
+
+	ImageMetadata.include_root_in_json = false if ImageMetadata.respond_to?(:include_root_in_json)
+
 end
