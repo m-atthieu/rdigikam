0c0371b08f82dbd543452685cd137b1baa6f6ae9
Matthieu DESILE <matthieu.desile@gmail.com>
2010-06-10 14:23:29 +0200

    album image controller first version
    this controller sends a list of images in an album

--- app/controllers/album_images_controller.rb
+++ app/controllers/album_images_controller.rb
@@ -1,2 +1,10 @@
 class AlbumImagesController < ApplicationController
+  def index
+    @album_images = AlbumImages.new params[:node]
+    respond_to do |format|
+      format.json {
+        render 'index.json.erb', :json => @album_images
+      }
+    end
+  end
 end
--- app/models/album_images.rb
+++ app/models/album_images.rb
@@ -1,5 +1,18 @@
 class AlbumImages
-	def new(node)
-		@album_root = node.scan(/[:num:]+/).first
+	attr_reader :images
+
+	def initialize(node)
+		@images = Array.new
+		if node != 'root' then
+			album_root = node.scan(/[0-9]+/).first
+			print "#{album_root}\n"
+			path = node.sub(album_root, '')
+			print "#{path}\n"
+			# recuperer l'id de l'album
+			album = Album.find :first, :conditions => ["albumRoot = ? and relativePath = ?", album_root, path]
+			album.images.each do |image|
+				@images << image
+			end
+		end
 	end
 end
